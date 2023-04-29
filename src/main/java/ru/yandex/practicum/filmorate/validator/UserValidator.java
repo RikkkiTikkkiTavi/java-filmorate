92902785ebc88cd92ca26a52e51a23a827517370
Constantin <Rikkki.Tikkki.Tavi@yandex.ru>
2023-04-30 00:56:04 +0300

    first commit

--- src/main/java/ru/yandex/practicum/filmorate/validator/UserValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/UserValidator.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.validator;
 
 import lombok.extern.slf4j.Slf4j;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
@@ -15,19 +14,19 @@ public class UserValidator {
         if (user.getName() == null) {
             user.setName(user.getLogin());
         }
-            if (user.getEmail().isEmpty() || !user.getEmail().contains("@")) {
-                throw new ValidationException("Электронная почта не может быть пустой и должна содержать символ @");
-            }
-            if (user.getLogin().isEmpty() || user.getLogin().contains(" ")) {
-                throw new ValidationException("Логин не может быть пустым и содержать пробелы");
-            }
-            if (user.getBirthday().isAfter(LocalDate.now())) {
-                throw new ValidationException("Дата рождения не может быть в будущем");
-            }
+        if (user.getEmail().isEmpty() || !user.getEmail().contains("@")) {
+            throw new ValidationException("Электронная почта не может быть пустой и должна содержать символ @");
         }
+        if (user.getLogin().isEmpty() || user.getLogin().contains(" ")) {
+            throw new ValidationException("Логин не может быть пустым и содержать пробелы");
+        }
+        if (user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ValidationException("Дата рождения не может быть в будущем");
+        }
+    }
 
     public static void checkId(Map<Integer, User> users, User user) {
-        if(!users.containsKey(user.getId())) {
+        if (!users.containsKey(user.getId())) {
             throw new ValidationException("Обновить пользователя с id:" + user.getId() + " невозможно по причине его отсутствия");
         }
     }
