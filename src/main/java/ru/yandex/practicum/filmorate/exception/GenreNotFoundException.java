57c1ef47532f218c8259357d505effd028979aae
Constantin <Rikkki.Tikkki.Tavi@yandex.ru>
2023-06-08 02:03:29 +0300

    add:dB

--- src/main/java/ru/yandex/practicum/filmorate/exception/GenreNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/GenreNotFoundException.java
@@ -2,6 +2,7 @@
 package ru.yandex.practicum.filmorate.exception;
 
 public class GenreNotFoundException extends RuntimeException {
+
     public GenreNotFoundException(String message) {
         super(message);
     }
